6b195c481a2b4e95f817a7a098441de31f6a6c5a
Paulo Renato <ficksjr@gmail.com>
2021-06-21 19:26:47 -0300

    commits iniciais do yarn

---
+++ index.js
@@ -0,0 +1,4 @@
+function enviarEmail(name, email) {
+    console.log("Enviar email para " + name + " com email " + email);
+}
+enviarEmail("Renato", "ficksjr@gmail.com");
---
+++ index.ts
@@ -0,0 +1,5 @@
+function enviarEmail(name: string, email: string){
+  console.log(`Enviar email para ${name} com email ${email}`)
+}
+
+enviarEmail("Renato", "ficksjr@gmail.com")
